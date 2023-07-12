4929b7ca7a97b4ce72c4bfcad90b405521b9ed53
Rishad Alam <rishad.s.a@gmail.com>
2023-07-12 19:27:44 -0400

    add photoitmes in isolation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
+import PhotoList from "./components/PhotoList";
+
+const photos = [];
+
+for (let i = 0; i < 3; i++) {
+  photos.push(<PhotoListItem key={i} />);
+}
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+const App = () => <div className="App">{photos}</div>;
 
-export default App
+export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,36 @@
+import React from "react";
 
-import React from 'react';
+import "../styles/PhotoListItem.scss";
 
-import '../styles/PhotoListItem.scss';
+const PhotoListItem = ({ id, imageSource, profile, username, location }) => {
+  // const { id, imageSource, profile, username, location } = props;
+  // const photoItem = {id: {id, imageSource, profile, username, location}}
 
-const PhotoListItem = () => {
-  /* Insert React */
-}
+  return (
+    <li key={id} className="photo-list__item">
+      <img src={imageSource} className="photo-list__image" />
+      <footer>
+        <img src={profile} />
+        <section className="photo-list__user-profile">
+          <p className="photo-list__user-info">{username}</p>
+          <p className="photo-list__user-location ">
+            {location.city}, {location.country}
+          </p>
+        </section>
+      </footer>
+    </li>
+  );
+};
 
 PhotoListItem.defaultProps = {
-  "id": "1",
-  "location": {
-    "city": "Montreal",
-    "country": "Canada"
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
   },
-  "imageSource": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  "username": "Joe Example",
-  "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-}
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
-export default PhotoListItem
+export default PhotoListItem;
